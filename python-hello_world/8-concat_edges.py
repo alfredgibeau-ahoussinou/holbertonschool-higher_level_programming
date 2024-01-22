72ae90dd3380e2cfb427ed8076d6e5801417e1b4
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2024-01-22 16:53:21 +0100

    All project

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming \
+language that combines remarkable power with very clear syntax"
+str = str.split()
+print(f"{' '.join(str[5:7])} with Python")
+
