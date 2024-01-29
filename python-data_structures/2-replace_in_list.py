324fad1e36d2426f1c14033ba047ace3566518c5
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2024-01-29 17:20:10 +0100

    Replace element

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = my_list[:idx] + [element] + my_list[idx + 1:]
+    return new_list
