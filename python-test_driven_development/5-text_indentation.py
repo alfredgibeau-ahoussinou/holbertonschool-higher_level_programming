f4087164658d4336aef296996996dfc0be257641
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2024-02-09 13:33:11 +0100

    Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+"""defines function to print text with 2 newlines after '.' '?' or ':' chars"""
+
+
 def text_indentation(text):
-    if not isinstance(text, str):
+    """prints text with 2 newlines after '.' '?' or ':' chars"""
+    if type(text) is not str:
         raise TypeError("text must be a string")
-
-    punctuation = ['.', '?', ':']
-    result = ""
-
+    previous = ""
     for char in text:
-        result += char
-        if char in punctuation:
-            result += "\n\n"
-
-    print(result.strip())
+        # leading whitespace
+        if char is " " and char is text[0] and previous is "":
+            previous = "\n"
+            continue
+        # whitespaces after newline
+        if char is " " and previous is "\n":
+            continue
+        # matching character, print char, print newlines
+        if char is "." or char is "?" or char is ":":
+            print(char)
+            print()
+            previous = "\n"
+        else:
+            print(char, end="")
+            previous = char
