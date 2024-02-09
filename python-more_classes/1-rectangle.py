cb52ca07c60f7582685153583b8ca78f9f41531f
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2024-02-09 09:50:53 +0100

    projet avance

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,36 +1,74 @@
 #!/usr/bin/python3
-"""
-This is the "Rectangle"  module.
-
-This module provides a simple Rectangle class with attribute width and height.
-Default values of both attributes are 0.
+"""1-rectangle, built for Holberton Python project 0x08 task 1.
 """
 
 
 class Rectangle:
-    """A Rectangle class with attributes  width and height"""
+    """At this stage the class only creates private instance attributes by
+    taking in two arguments.
+
+    Args:
+        width (int): horizontal dimension of rectangle, defaults to 0
+        height (int): vertical dimension of rectangle, defaults to 0
+
+    """
     def __init__(self, width=0, height=0):
+        # attribute assigment here engages setters defined below
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """__width getter.
+
+        Returns:
+            __width (int): horizontal dimension of rectangle
+
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Args:
+            value (int): horizontal dimension of rectangle
+
+        Attributes:
+            __width (int): horizontal dimension of rectangle
+
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+
+        """
         if type(value) is not int:
             raise TypeError('width must be an integer')
-        if value < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
     def height(self):
+        """__height getter.
+
+        Returns:
+            __height (int): vertical dimension of rectangle
+
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Args:
+            value (int): vertical dimension of rectangle
+
+        Attributes:
+            __height (int): vertical dimension of rectangle
+
+        Raises:
+            TypeError: If `value` is not an int.
+            ValueError: If `value` is less than 0.
+
+        """
         if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
