c3da3199858ccaf1f31dc01acbc05c0b0ce73074
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2024-02-12 19:26:44 +0100

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+Attribute lookup
+"""
+
+
 def lookup(obj):
-    return dir(obj)
+    """Lookup all attributes in Class
+
+    Args:
+        obj: object class
+    Returns:
+        Na
+    """
+    return [i for i in dir(obj)]
