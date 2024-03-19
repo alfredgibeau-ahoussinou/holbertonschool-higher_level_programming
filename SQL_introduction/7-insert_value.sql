6348e8d9bc3984529e7051dc6fd35380241f68e2
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2024-03-19 15:16:19 +0100

    7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,4 @@
 -- Inserts a new row in a table
 -- Query to insert a new row in the table first_table
-INSERT INTO first_table (id, name) VALUES (89, "Holberton School");
+INSERT INTO hbtn_0c_0.first_table (id, name)
+VALUES (89, 'Best School');
